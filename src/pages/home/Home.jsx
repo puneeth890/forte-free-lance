9d2912e15aac152b0d24565d85bd57e7839dd78d
Asritha Pallapotu <asrithapallapotu@gmail.com>
2025-02-16 18:01:06 -0500

    home components- featured, trustedBy, slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import "./Home.scss"
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import Slide from "../../components/slide/Slide";
+import CatCard from "../../components/catCard/CatCard";
+import { cards, projects } from "../../data";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowsScroll={5}>
+        {cards.map((card) => (
+          <CatCard key={card.id} card={card} />
+        ))}
+      </Slide>
+      </div>
   )
 }
 
