905deac55611dd14c21428ce8ca6940121d303d7
Asritha Pallapotu <asrithapallapotu@gmail.com>
2025-03-09 14:51:28 -0400

    authentication with jwt bcrypt and cookies

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
+import gigRoute from "./routes/gig.route.js";
+import orderRoute from "./routes/order.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
+import reviewRoute from "./routes/review.route.js";
+import authRoute from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app= express()
 
@@ -17,9 +25,27 @@ const connect = async () => {
     } catch (error) {
       console.log(error);
     }
-  };
+};
 
-  app.listen(8800,()=>{
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute)
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(8800,()=>{
     connect();
     console.log("Backend server started")
 })
